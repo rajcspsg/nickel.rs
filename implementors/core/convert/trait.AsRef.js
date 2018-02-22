b7f1598aee82771679c779282177b7aca377034c
Travis CI User <travis@example.org>
2018-02-22 21:40:37 +0000

    Documentation for nickel-org/nickel.rs@a61067a

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"hyper/method/enum.Method.html\" title=\"enum hyper::method::Method\">Method</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"hyper/version/enum.HttpVersion.html\" title=\"enum hyper::version::HttpVersion\">HttpVersion</a>",];
+implementors["regex_syntax"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">[</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.slice.html\">]</a>&gt; for <a class=\"struct\" href=\"regex_syntax/struct.Lit.html\" title=\"struct regex_syntax::Lit\">Lit</a>",];
+implementors["unicase"] = ["impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"unicase/struct.UniCase.html\" title=\"struct unicase::UniCase\">UniCase</a>&lt;S&gt;",];
+implementors["url"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"url/struct.Url.html\" title=\"struct url::Url\">Url</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/plugin/trait.Pluggable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nickel"] = ["impl&lt;'mw, 'server, D&gt; <a class=\"trait\" href=\"plugin/trait.Pluggable.html\" title=\"trait plugin::Pluggable\">Pluggable</a> for <a class=\"struct\" href=\"nickel/struct.Request.html\" title=\"struct nickel::Request\">Request</a>&lt;'mw, 'server, D&gt;","impl&lt;'a, D, T:&nbsp;'static + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/any/trait.Any.html\" title=\"trait core::any::Any\">Any</a>&gt; <a class=\"trait\" href=\"plugin/trait.Pluggable.html\" title=\"trait plugin::Pluggable\">Pluggable</a> for <a class=\"struct\" href=\"nickel/struct.Response.html\" title=\"struct nickel::Response\">Response</a>&lt;'a, D, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
